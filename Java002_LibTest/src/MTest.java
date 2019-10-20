d82007b23f88d01a6b9db5986e415c818b397a17
woohams <50380276+woohams@users.noreply.github.com>
2019-10-21 00:13:38 +0900

    Add files via upload

---
+++ Java002_LibTest/src/MTest.java
@@ -0,0 +1,13 @@
+import com.spring.Score;
+
+public class MTest {
+
+	public static void main(String[] args) {
+		Score s = new Score();
+		s.setName("바보");
+		s.setKor(100);
+		s.setEng(80);
+		s.setMath(91);
+		System.out.println(s);
+	}
+}
